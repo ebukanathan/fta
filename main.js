5b25ac1a62115f2a371e1620f3e8185ff633df37
ebuka nathan <ebukanathan@gmail.com>
2023-05-08 17:52:08 +0100

    mobile menu

--- main.js
+++ main.js
@@ -168,34 +168,34 @@ const swiper = new Swiper('.swiper', {
   });
 
 
-  // Modal
-  // Get the modal
-var modal = document.getElementById("myModal");
+//   // Modal
+//   // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-  console.log('clicked')
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+//   console.log('clicked')
   
-}
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// } 
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu');
@@ -206,6 +206,7 @@ const navClose = document.getElementById('nav-close')
 if(navToggle){
     navToggle.addEventListener('click',()=>{
         navMenu.classList.add('show-menu')
+        console.log('hello people')
       
         
     })
@@ -215,6 +216,7 @@ if(navToggle){
 if(navClose){
     navClose.addEventListener('click',()=>{
         navMenu.classList.remove('show-menu')
+        
     })
 }
 
